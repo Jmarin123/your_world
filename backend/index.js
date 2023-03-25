cc1843808bfa6274531f5d987f1b0abd21949f98
Jason Osorio <jomarin002@gmail.com>
2023-03-25 16:29:37 -0400

    large changes with basic html routes

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+
+const dbUrl = process.env.MONGO_URL || "mongodb://localhost:27017/your_world"
+mongoose.connect(dbUrl);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new passportLocal(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
+})
