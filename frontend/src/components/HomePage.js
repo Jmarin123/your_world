4bc6364658d091d35a4bdee8f570c16b2dfd016d
Rossnunez <nrosseleen@gmail.com>
2023-04-08 01:56:02 -0400

    completed public map listing page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,21 +1,27 @@
 // import Map from './Map.js';
 import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import MapCard from './MapCard.js'
 
 export default function AppBanner() {
 
     return (
         <Box sx={{ flexGrow: 1 }} id="homePageBackground">
-            
             <Box id="publicBox" component="form" noValidate >
-            <section id="public">Public Map Listing</section>
-            <div id='line'></div>
+                <section id="public">Public Map Listing</section>
+                <div id='line'></div>
+
+                <List id="map-cards-list" sx={{display: 'flex'}}>
+                    <MapCard/>
+                    <MapCard/>
+                    <MapCard/>
+                    <MapCard/>
+                    <MapCard/>
+                    <MapCard/>
+                    <MapCard/>
+                    <MapCard/>
+                </List>
             </Box>
-
-
-            {/* <Box id="box">
-                <Map />
-            </Box> */}
         </Box>
     );
 }
-
---
+++ frontend/src/components/MapCard.js
@@ -0,0 +1,68 @@
+import React, { useContext, useState } from 'react'
+import { GlobalStoreContext } from '../store'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import MapCardSample from './/mapcardsample.jpg'
+import IconButton from '@mui/material/IconButton';
+import DownloadIcon from '@mui/icons-material/Download';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+
+export default function MapCard() {
+
+    let cardClass = "list-card unselected-list-card";
+
+    let StyledIconButton = styled(IconButton)({
+        color: "black",
+        
+        '&:hover': {
+            opacity: 1,
+            transition: "color 0.7s, transform 0.7s",
+            transform: 'scale(1.1)',
+            // transitionDuration: '100ms',
+            color: '#FDE66B'
+        }
+    });
+
+
+    return (
+        <ListItem id='listItemMapCard' >
+            <div
+                key={1}
+                id="mapCard1"
+                className={cardClass}
+            >
+                <div id='cardTitle'>
+                    Atlantis
+                </div>
+
+                <div id='map-card-line'></div>
+                
+                <p id='map-card-author'>By: Author</p>
+
+                <img id="map-card-image" src={MapCardSample} alt="mapcardsample" />
+
+                <Box sx={{marginTop: '6%', marginLeft: '4%'}}>
+                    <StyledIconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        sx={{ mr: 2 }}
+                    >
+                        <DownloadIcon style={{ fontSize: "35px", float: "left" }} />
+                    </StyledIconButton>
+
+                    <StyledIconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        sx={{ mr: 2 }}
+                    >
+                        <FileCopyIcon style={{ fontSize: "35px", float: "left" }} />
+                    </StyledIconButton>
+                </Box>
+            </div>
+        </ListItem>
+    );
+
+}
