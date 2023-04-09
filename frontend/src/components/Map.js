ce361d96095cd9de25766bb5723296ebcca4ceea
Rossnunez <nrosseleen@gmail.com>
2023-04-09 18:08:22 -0400

    fixed the status bar

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -8,6 +8,7 @@ import { useContext, useState } from 'react'
 import { FeatureGroup } from 'react-leaflet';
 import { EditControl } from "react-leaflet-draw"
 import Box from '@mui/material/Box';
+import Statusbar from './Statusbar';
 
 export default function Map() {
   const { store } = useContext(GlobalStoreContext);
@@ -59,7 +60,8 @@ export default function Map() {
 
   return (
     <Box sx={{ flexGrow: 1 }} id="homePageBackground">
-    <Box id="publicBox" component="form" noValidate >
+
+    <Box id="mapBox" component="form" noValidate >
       <MapContainer style={{ height: "80vh" }} zoom={2} center={[20, 100]}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {
@@ -76,6 +78,10 @@ export default function Map() {
         value={color}
         onChange={colorChange}
       />
+      </Box>
+
+      <Box id="statusBox">
+        <Statusbar />
       </Box>
     </Box>
   );
