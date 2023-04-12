8c44f5c5a1a579a38b775d485a9759d876deb88e
chunwei <anniewei866@gmail.com>
2023-04-12 17:04:14 -0400

    Added deleteMap, getMapById and getMapPairs in backend

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -41,12 +41,12 @@ export default function Map() {
 
   let StyledIconButton = styled(IconButton)({
     color: "black",
-    
+
     '&:hover': {
-        opacity: 1,
-        transition: "color 0.7s, transform 0.7s",
-        transform: 'scale(1.1)',
-        color: '#FDE66B'
+      opacity: 1,
+      transition: "color 0.7s, transform 0.7s",
+      transform: 'scale(1.1)',
+      color: '#FDE66B'
     }
   });
 
@@ -93,8 +93,8 @@ export default function Map() {
 
   return (
     <Box sx={{ flexGrow: 1 }} id="homePageBackground">
-    
-    <Box id="editBox">
+
+      <Box id="editBox">
         <p id='map-edit-title'>Edit Panel</p>
         <div id="edit-line"></div>
         <Box id="editPanel">
@@ -102,178 +102,178 @@ export default function Map() {
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <ExploreIcon style={{ fontSize: "45px"}} titleAccess="Navigate" />
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <ExploreIcon style={{ fontSize: "45px" }} titleAccess="Navigate" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <SaveIcon style={{ fontSize: "45px"}} titleAccess="Save"/>
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <SaveIcon style={{ fontSize: "45px" }} titleAccess="Save" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <UndoIcon style={{ fontSize: "45px"}} titleAccess="Undo"/>
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <UndoIcon style={{ fontSize: "45px" }} titleAccess="Undo" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <RedoIcon style={{  fontSize: "45px"}} titleAccess="Redo" />
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <RedoIcon style={{ fontSize: "45px" }} titleAccess="Redo" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <CompressIcon style={{  fontSize: "45px"}} titleAccess="Compress"/>
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <CompressIcon style={{ fontSize: "45px" }} titleAccess="Compress" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <GridViewIcon style={{fontSize: "45px"}} titleAccess="Split" />
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <GridViewIcon style={{ fontSize: "45px" }} titleAccess="Split" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <MergeIcon style={{ fontSize: "45px"}} titleAccess="Merge"/>
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <MergeIcon style={{ fontSize: "45px" }} titleAccess="Merge" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <ColorLensIcon style={{ fontSize: "45px"}} titleAccess="Color Subregion"/>
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <ColorLensIcon style={{ fontSize: "45px" }} titleAccess="Color Subregion" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <FormatColorFillIcon style={{ fontSize: "45px"}} titleAccess="Color Background"/>
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <FormatColorFillIcon style={{ fontSize: "45px" }} titleAccess="Color Background" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <BorderColorIcon style={{fontSize: "45px"}} titleAccess="Color Border" />
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <BorderColorIcon style={{ fontSize: "45px" }} titleAccess="Color Border" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <EmojiFlagsIcon style={{fontSize: "45px"}} titleAccess="Edit Legends"/>
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <EmojiFlagsIcon style={{ fontSize: "45px" }} titleAccess="Edit Legends" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{flex: "1 0 50%", marginBottom: "10px"}}
-            >
-              <CreateIcon style={{fontSize: "45px"}} titleAccess="Edit Text"/>
-            </StyledIconButton>
+            sx={{ flex: "1 0 50%", marginBottom: "10px" }}
+          >
+            <CreateIcon style={{ fontSize: "45px" }} titleAccess="Edit Text" />
+          </StyledIconButton>
 
-            <StyledIconButton
+          <StyledIconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{marginBottom: "10px"}}
-            >
-              <TitleIcon style={{fontSize: "45px", float: "left"}} titleAccess="Insert Text"/>
-            </StyledIconButton>
+            sx={{ marginBottom: "10px" }}
+          >
+            <TitleIcon style={{ fontSize: "45px", float: "left" }} titleAccess="Insert Text" />
+          </StyledIconButton>
         </Box>
         <div id="edit-line2"></div>
         <br />
-        <FormControl variant="standard" sx={{ 
-          m: 1, 
+        <FormControl variant="standard" sx={{
+          m: 1,
           minWidth: 120,
-          '& > :not(style)': {backgroundColor: "#D9D9D9", marginTop: '0.75%'},
+          '& > :not(style)': { backgroundColor: "#D9D9D9", marginTop: '0.75%' },
           "& .css-m5hdmq-MuiInputBase-root-MuiInput-root-MuiSelect-root:after": {
-              borderColor: '#FDE66B'
-            }, 
-          "& label.Mui-focused": {
-              color: '#756060' //purple
+            borderColor: '#FDE66B'
           },
-          }}>
-        <InputLabel id="demo-simple-select-standard-label">Font</InputLabel>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={font}
-          onChange={handleChange}
-          label="Font"
-          defaultValue={font}
-        >
-          <MenuItem value={"Arial"}>Arial</MenuItem>
-          <MenuItem value={"New Times Roman"}>New Times Roman</MenuItem>
-          <MenuItem value={"Comic Sans"}>Comic Sans</MenuItem>
-        </Select>
-      </FormControl>
-      <div id="edit-line3"></div>
+          "& label.Mui-focused": {
+            color: '#756060' //purple
+          },
+        }}>
+          <InputLabel id="demo-simple-select-standard-label">Font</InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={font}
+            onChange={handleChange}
+            label="Font"
+            defaultValue={font}
+          >
+            <MenuItem value={"Arial"}>Arial</MenuItem>
+            <MenuItem value={"New Times Roman"}>New Times Roman</MenuItem>
+            <MenuItem value={"Comic Sans"}>Comic Sans</MenuItem>
+          </Select>
+        </FormControl>
+        <div id="edit-line3"></div>
         <br />
-      <Button id="publishButton" type="submit" sx={{textTransform: `none`}}>
-        <p id="text">Publish</p>
-      </Button>
+        <Button id="publishButton" type="submit" sx={{ textTransform: `none` }}>
+          <p id="text">Publish</p>
+        </Button>
       </Box>
 
 
 
 
 
-    <Box id="statusBoxEdit">
-      <Statusbar />
-    </Box>
+      <Box id="statusBoxEdit">
+        <Statusbar />
+      </Box>
 
-    <Box id="mapBoxEdit" component="form" noValidate >
-      <MapContainer style={{ height: "80vh" }} zoom={2} center={[20, 100]}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {
-          store.currentMap ? renderedMap : <div></div>
-        }
-        <FeatureGroup>
-          <EditControl
-            position='topright'
-          />
-        </FeatureGroup>
-      </MapContainer>
-      <input
-        type="color"
-        value={color}
-        onChange={colorChange}
-      />
+      <Box id="mapBoxEdit" component="form" noValidate >
+        <MapContainer style={{ height: "80vh" }} zoom={2} center={[20, 100]}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          {
+            store.currentMap ? renderedMap : <div></div>
+          }
+          <FeatureGroup>
+            <EditControl
+              position='topright'
+            />
+          </FeatureGroup>
+        </MapContainer>
+        <input
+          type="color"
+          value={color}
+          onChange={colorChange}
+        />
       </Box>
     </Box>
   );
