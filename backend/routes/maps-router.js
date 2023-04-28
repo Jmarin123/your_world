cda6e45d34feb3cfabbacb808c7bc71c1c220c58
Jason Osorio <jomarin002@gmail.com>
2023-04-27 22:23:23 -0400

    removed auth for the time being

--- backend/routes/maps-router.js
+++ backend/routes/maps-router.js
@@ -3,8 +3,8 @@ const MapController = require('../controllers/map-controller')
 const router = express.Router()
 const auth = require('../auth')
 
-router.post('/map', auth.verify, MapController.createMap)
-router.delete('/map/:id', auth.verify, MapController.deleteMap)
+router.post('/map', MapController.createMap) // auth.verify
+router.delete('/map/:id', MapController.deleteMap) //auth.verify
 router.get('/map/:id', MapController.getMapById)
 router.get('/maps', MapController.getAllMaps)
 router.put('/map/:id', MapController.updateMap)
