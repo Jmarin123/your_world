ead55921b7d3487a7f387d76e11dc5fefedc0bb9
Jason Osorio <jomarin002@gmail.com>
2023-05-01 19:37:17 -0400

    m

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -149,7 +149,7 @@ export default function Map() {
     setCompressionStatus(event.target.value);
   };
   let compressModal = <Modal
-  open={store.currentModal === "COMPRESS_MAP"}
+    open={store.currentModal === "COMPRESS_MAP"}
   >
     <Grid container sx={styleCompress}>
       <Grid container item >
@@ -161,21 +161,21 @@ export default function Map() {
         <Box>
           <Typography id="modal-text" xs={4} sx={{ textAlign: `center` }}>Are you sure you want to permanently compress your Map?</Typography>
           <Typography id="modal-text" xs={4}>*Once you confirm your changes, you cannot undo it, changes are permanet!</Typography>
-          <RadioGroup row value={compressionStatus} onChange={handleRadioChange} sx={{justifyContent: 'center', alignItems: 'center'}}>
-          <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'nowrap',  justifyContent: 'center', alignItems: 'center'}}>
-            <FormControlLabel value="normal" control={<Radio />} label="Normal" labelPlacement="bottom" />
-            <FormControlLabel
-              value="medium_compressed"
-              control={<Radio />}
-              label="medium"
-              labelPlacement="bottom"
-            />
-            <FormControlLabel
-              value="fully_compressed"
-              control={<Radio />}
-              label="fully"
-              labelPlacement="bottom"
-            />
+          <RadioGroup row value={compressionStatus} onChange={handleRadioChange} sx={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'nowrap', justifyContent: 'center', alignItems: 'center' }}>
+              <FormControlLabel value="normal" control={<Radio />} label="Normal" labelPlacement="bottom" />
+              <FormControlLabel
+                value="medium_compressed"
+                control={<Radio />}
+                label="medium"
+                labelPlacement="bottom"
+              />
+              <FormControlLabel
+                value="fully_compressed"
+                control={<Radio />}
+                label="fully"
+                labelPlacement="bottom"
+              />
             </Box>
           </RadioGroup>
         </Box>
@@ -191,9 +191,9 @@ export default function Map() {
     setMaplayout(newMap ? renderedMap : <div></div>)
   }
   function handleConfirmCompress() {
-    if(compressionStatus === "normal") {
+    if (compressionStatus === "normal") {
       setCompressValue(0)
-    } else if(compressionStatus === "medium_compressed"){
+    } else if (compressionStatus === "medium_compressed") {
       setCompressValue(0.5)
     } else {
       setCompressValue(1)
@@ -203,8 +203,8 @@ export default function Map() {
     //compressFlag = true
     store.compressMap();
   }
-  function markCompression() { 
-    if(!store.compressStatus){
+  function markCompression() {
+    if (!store.compressStatus) {
       setMaplayout(<div></div>)
       store.markCompression()
     }
@@ -256,6 +256,8 @@ export default function Map() {
   }
 
   function clickFeature(event) {
+    console.log("HEY IT CLICKED HERE");
+    console.log(event.target);
     if (mergeFLAG) { // Merge active. Mark region if first, and merge if second. 
       event.target.setStyle({
         color: "#000000",
@@ -281,7 +283,6 @@ export default function Map() {
     setMaplayout(<div></div>)
     store.markSubregion(event.target.feature)
   }
-
   const onEachCountry = (country, layer) => {
     layer.on({
       click: clickFeature,
@@ -293,7 +294,6 @@ export default function Map() {
     }
     layer.bindPopup(popupContent);
   };
-
   function colorChange(event) {
     //setColor(event.target.value);
     colorFill = event.target.value
@@ -318,7 +318,7 @@ export default function Map() {
     setMaplayout(<FeatureGroup>
       {newMap && newMap.features.map((feature, index) => {
         if (feature.geometry.type === 'Polygon') {
-          return <Polygon key={index} positions={feature.geometry.coordinates[0]} myCustomKeyProp={index + ""} polyName={feature.properties.admin}/>;
+          return <Polygon key={index} positions={feature.geometry.coordinates[0]} myCustomKeyProp={index + ""} polyName={feature.properties.admin} />;
         } else if (feature.geometry.type === 'MultiPolygon') {
           const polygons = feature.geometry.coordinates.map((polygonCoords, polygonIndex) => (
             <Polygon key={polygonIndex} positions={polygonCoords[0]} myCustomKeyProp={index + "-" + polygonIndex} polyName={feature.properties.admin} />
@@ -339,7 +339,7 @@ export default function Map() {
 
   //THIS IS FOR MAP MODE SWITCHING AKA NAVIGATION
   useEffect(() => {
-    if(MapLayOutFLAG === 1) {
+    if (MapLayOutFLAG === 1) {
       //var options = {tolerance: compressValue, highQuality: false};
       //var simplifiedMap = turf.simplify(newMap, options);
       setMaplayout(<FeatureGroup>
@@ -368,7 +368,7 @@ export default function Map() {
 
   //THIS IS FOR MAP COMPRESSION
   useEffect(() => {
-    let options = {tolerance: compressValue, highQuality: false};
+    let options = { tolerance: compressValue, highQuality: false };
     // eslint-disable-next-line
     newMap = turf.simplify(newMap, options);
     //setNewMap(turf.simplify(newMap, options));
@@ -430,7 +430,7 @@ export default function Map() {
         mergeFeatureFlag = null
         mergeFLAG = 0
       }
-      else if(mergeFLAG){
+      else if (mergeFLAG) {
         setMergeFeature(null)
         setMergeFeature_1(null)
         mergeFeatureFlag = null
@@ -451,16 +451,16 @@ export default function Map() {
       setMergedFlag(false)
       geoJsonLayer.current.clearLayers().addData(store.currentMap.dataFromMap.features);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [newMap]);
 
   useEffect(() => {
-    if(geoJsonLayer.current){
+    if (geoJsonLayer.current) {
       geoJsonLayer.current.resetStyle()
     }
-    if(selectedFeature){
+    if (selectedFeature) {
       selectedFeature.setStyle({
-        fillColor : colorFill
+        fillColor: colorFill
       })
     }
   }, [selectedFeature]);
@@ -471,25 +471,25 @@ export default function Map() {
       const editedKey = layer.options.myCustomKeyProp;
       let newFeature = layer.toGeoJSON();
       const editedName = layer.options.polyName
-      
+
       if (editedKey.includes('-')) { //if a '-' is included, this means its a multipolygon -3- 
         //const parts = editedKey.split("-"); //parts = ["index of subregion", "index of subregion in multipolygon"]
         //let index = parseInt(parts[0]);
         //let index2 = parseInt(parts[1]);
         //if((index < lengthOfMap)){
-        for(let i = 0; i < store.currentMap.dataFromMap.features.length; i++){
-          if(editedName === store.currentMap.dataFromMap.features[i].properties.admin){
+        for (let i = 0; i < store.currentMap.dataFromMap.features.length; i++) {
+          if (editedName === store.currentMap.dataFromMap.features[i].properties.admin) {
             let featureFound = store.currentMap.dataFromMap.features[i]
-            for(let j = 0; j < featureFound.geometry.coordinates.length; j++){
-            
+            for (let j = 0; j < featureFound.geometry.coordinates.length; j++) {
+
               let oldFeature = JSON.parse(JSON.stringify(featureFound));
-              
+
               let polygon1 = turf.polygon(newFeature.geometry.coordinates);
               let polygon2 = turf.polygon(oldFeature.geometry.coordinates[j]);
-              const polygonRounded = turf.truncate(polygon1, {precision: 3});
-              const polygonRounded2 = turf.truncate(polygon2, {precision: 3});
-              
-              if(turf.booleanEqual(polygonRounded, polygonRounded2)){
+              const polygonRounded = turf.truncate(polygon1, { precision: 3 });
+              const polygonRounded2 = turf.truncate(polygon2, { precision: 3 });
+
+              if (turf.booleanEqual(polygonRounded, polygonRounded2)) {
                 //store.editCurrentMapVertex(editedKey, newFeature.geometry.coordinates, oldFeature.geometry.coordinates);
                 console.log("we deleted the multipolygon feature!")
                 store.currentMap.dataFromMap.features[i].geometry.coordinates.splice(j, 1);
@@ -501,64 +501,64 @@ export default function Map() {
         }
       } else { //if NO '-' than this means its a Polygon
         //if((editedKey < lengthOfMap)){
-          for(let i = 0; i < store.currentMap.dataFromMap.features.length; i++){
-            if(editedName === store.currentMap.dataFromMap.features[i].properties.admin){
+        for (let i = 0; i < store.currentMap.dataFromMap.features.length; i++) {
+          if (editedName === store.currentMap.dataFromMap.features[i].properties.admin) {
 
-              let featureFound = store.currentMap.dataFromMap.features[i]
-              let oldFeature = JSON.parse(JSON.stringify(featureFound)); //create a deep copy
+            let featureFound = store.currentMap.dataFromMap.features[i]
+            let oldFeature = JSON.parse(JSON.stringify(featureFound)); //create a deep copy
 
-              let polygon1 = turf.polygon(newFeature.geometry.coordinates);
-              let polygon2 = turf.polygon(oldFeature.geometry.coordinates);
+            let polygon1 = turf.polygon(newFeature.geometry.coordinates);
+            let polygon2 = turf.polygon(oldFeature.geometry.coordinates);
 
-              if(turf.booleanEqual(polygon1, polygon2)){
-                console.log("we deleted the feature!")
-                //store.editCurrentMapVertex(editedKey, newFeature.geometry.coordinates, oldFeature.geometry.coordinates);
-                store.currentMap.dataFromMap.features.splice(i, 1);
-                store.deleteSubregion();
-                break;
-              }
+            if (turf.booleanEqual(polygon1, polygon2)) {
+              console.log("we deleted the feature!")
+              //store.editCurrentMapVertex(editedKey, newFeature.geometry.coordinates, oldFeature.geometry.coordinates);
+              store.currentMap.dataFromMap.features.splice(i, 1);
+              store.deleteSubregion();
+              break;
             }
           }
+        }
         //}
       }
     });
   }
 
   const handleEditable = (e) => {
-    
+
     const layers = e.layers;
     layers.eachLayer(layer => { //ignore dis 4 now .3.
-    
-    //let editedLayer = e.layers.getLayers()[0];
-    //const editedKey = layer.options.myCustomKeyProp; //gets the special key attached to each <Polygon> to see what country the Poly belongs to in the GEOJSON file
-    //layer = turf.flip(layer.toGeoJSON()); //we need to flip the [long, lat] coordinates to [lat, long] FIRST, cause it wont render properly. then convert the layer to a geojson object
+
+      //let editedLayer = e.layers.getLayers()[0];
+      //const editedKey = layer.options.myCustomKeyProp; //gets the special key attached to each <Polygon> to see what country the Poly belongs to in the GEOJSON file
+      //layer = turf.flip(layer.toGeoJSON()); //we need to flip the [long, lat] coordinates to [lat, long] FIRST, cause it wont render properly. then convert the layer to a geojson object
 
 
-    const editedKey = layer.options.myCustomKeyProp;
-    let newFeature = layer.toGeoJSON();
+      const editedKey = layer.options.myCustomKeyProp;
+      let newFeature = layer.toGeoJSON();
 
-    if (editedKey.includes('-')) { //if a '-' is included, this means its a multipolygon -3- 
-      const parts = editedKey.split("-"); //parts = ["index of subregion", "index of subregion in multipolygon"]
-      let featureFound = store.currentMap.dataFromMap.features[parts[0]]
-      let oldFeature = JSON.parse(JSON.stringify(featureFound));
+      if (editedKey.includes('-')) { //if a '-' is included, this means its a multipolygon -3- 
+        const parts = editedKey.split("-"); //parts = ["index of subregion", "index of subregion in multipolygon"]
+        let featureFound = store.currentMap.dataFromMap.features[parts[0]]
+        let oldFeature = JSON.parse(JSON.stringify(featureFound));
 
-      let polygon1 = turf.polygon(newFeature.geometry.coordinates);
-      let polygon2 = turf.polygon(oldFeature.geometry.coordinates[parts[1]]);
-      
-      if(!turf.booleanEqual(polygon1, polygon2)){
-        store.editCurrentMapVertex(editedKey, newFeature.geometry.coordinates, oldFeature.geometry.coordinates);
+        let polygon1 = turf.polygon(newFeature.geometry.coordinates);
+        let polygon2 = turf.polygon(oldFeature.geometry.coordinates[parts[1]]);
+
+        if (!turf.booleanEqual(polygon1, polygon2)) {
+          store.editCurrentMapVertex(editedKey, newFeature.geometry.coordinates, oldFeature.geometry.coordinates);
+        }
+      } else { //if NO '-' than this means its a Polygon
+        let featureFound = store.currentMap.dataFromMap.features[editedKey]
+        let oldFeature = JSON.parse(JSON.stringify(featureFound)); //create a deep copy
+
+        let polygon1 = turf.polygon(newFeature.geometry.coordinates);
+        let polygon2 = turf.polygon(oldFeature.geometry.coordinates);
+
+        if (!turf.booleanEqual(polygon1, polygon2)) {
+          store.editCurrentMapVertex(editedKey, newFeature.geometry.coordinates, oldFeature.geometry.coordinates);
+        }
       }
-    } else { //if NO '-' than this means its a Polygon
-      let featureFound = store.currentMap.dataFromMap.features[editedKey]
-      let oldFeature = JSON.parse(JSON.stringify(featureFound)); //create a deep copy
-
-      let polygon1 = turf.polygon(newFeature.geometry.coordinates);
-      let polygon2 = turf.polygon(oldFeature.geometry.coordinates);
-
-      if(!turf.booleanEqual(polygon1, polygon2)){
-        store.editCurrentMapVertex(editedKey, newFeature.geometry.coordinates, oldFeature.geometry.coordinates);
-      }
-    }
     });
   };
 
@@ -613,7 +613,7 @@ export default function Map() {
             aria-label="open drawer"
             sx={{ flex: "1 0 50%", marginBottom: "10px" }}
           >
-            <Compress style={{ fontSize: "45px" }} titleAccess="Compress" onClick={markCompression}/>
+            <Compress style={{ fontSize: "45px" }} titleAccess="Compress" onClick={markCompression} />
           </StyledIconButton>
 
           <StyledIconButton
@@ -743,8 +743,10 @@ export default function Map() {
           onChange={colorChange}
         />
       </Box>
+
       {modal}
       {compressModal}
+      <textarea id="textarea" cols="30" rows="10" placeholder="write here..."></textarea>
     </Box>
   );
 }
